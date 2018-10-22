2159b6ad2ccd3113413b69c3b7c57b65a4a2f573
KimSunUk <33857465+KimSunUk@users.noreply.github.com>
2018-10-22 22:50:13 +0900

    add # for test

--- string.py
+++ string.py
@@ -1,23 +1,24 @@
 # python exercise #1 : data manipulation
 
-a = input()
+a = input('aaa')
 b = input()
 
 # a와 b의 길이를 출력
 
 print(a.len())
+print("Hello")
 
 # a와 b의 왼쪽, 오른쪽에 있는 빈칸 제거
 
 
 
 # a와 b를 대문자로 변경
-A = 
-B = 
+#A =
+#B =
 
 
 # A와 B를 결합하여 C를 만듬
-C = A + B
+#C = A + B
 
 
 # C를 출력
